9ef16fda5a3135fa6087c8ebff550a03381da4b9
KengoWada <louiskengo3@gmail.com>
2018-12-15 20:44:07 +0300

    Modified db.py

--- db.py
+++ db.py
@@ -10,22 +10,22 @@ class DatabaseConnection:
         if os.getenv('DB_NAME') == 'test_db':
             self.db_name = 'test_db'
         else:
-            self.db_name = 'd9ke6glsa2ksgu'
+            self.db_name = 'learn_db'
 
         pprint(self.db_name)
 
         try:
             self.connection = psycopg2.connect(
-                dbname=self.db_name, user='oxvqoqgwylhbeq', host='ec2-50-19-249-121.compute-1.amazonaws.com', password='22df9dcb6c71a27256acbad9e5353dd6a9fc8176a4a39ae320eb5d03cad1002b', port=5432
+                dbname=self.db_name, user='postgres', host='localhost', password='kengo1234', port=5432
             )
             self.connection.autocommit = True
-            self.cur = self.connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+            self.cursor = self.connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
             print('Connected to the database successfully.')
 
             create_users_table = "CREATE TABLE IF NOT EXISTS users (userId SERIAL NOT NULL PRIMARY KEY, username TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL);"
 
-            self.cur.execute(create_users_table)
+            self.cursor.execute(create_users_table)
         except Exception as e:
             pprint(e)
             pprint('Failed to connect to the database.')
@@ -33,33 +33,33 @@ class DatabaseConnection:
     def register_user(self, username, email, password):
         reg_user = f"INSERT INTO users(username, email, password) VALUES('{username}', '{email}', '{password}');"
         pprint(reg_user)
-        self.cur.execute(reg_user)
+        self.cursor.execute(reg_user)
     
     def check_username(self, username):
         query = f"SELECT * FROM users WHERE username='{username}';"
         pprint(query)
-        self.cur.execute(query)
-        user = self.cur.fetchone()
+        self.cursor.execute(query)
+        user = self.cursor.fetchone()
         return user
     
     def check_email(self, email):
         query = f"SELECT * FROM users WHERE email='{email}';"
         pprint(query)
-        self.cur.execute(query)
-        user = self.cur.fetchone()
+        self.cursor.execute(query)
+        user = self.cursor.fetchone()
         return user
 
     def login(self, username):
         query = f"SELECT * FROM users WHERE username='{username}';"
         pprint(query)
-        self.cur.execute(query)
-        user = self.cur.fetchone()
+        self.cursor.execute(query)
+        user = self.cursor.fetchone()
         pprint(user)
         return user
 
     def drop_table(self, table_name):
         drop = f"DROP TABLE {table_name};"
-        self.cur.execute(drop)
+        self.cursor.execute(drop)
 
 
 if __name__ == '__main__':
